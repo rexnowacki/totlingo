ba39a89b3694a281d782e92550156e6da77c2a9c
Rex Nowacki <cnowacki@imac.lan>
2023-05-03 17:47:55 -0700

    Added comments for class ChildProfile

--- src/totlingo.py
+++ src/totlingo.py
@@ -4,28 +4,34 @@ import datetime
 import shutil
 
 
-
+"""The ChildProfile class represents a child's profile and provides functions
+to create, remove, or log-in to a child's profile."""
 class ChildProfile:
-    def __init__(self, name, birth_date):
+    def __init__(self, name):
         self.name = name.lower()
-        self.birth_date = birth_date
-
+        # Creates a new directory with the child's name if it does not exist 
         if not os.path.exists(self.name):
             os.makedirs(self.name)
 
+    # Static method to create a new child profile
     @staticmethod
     def create_new_profile():
+        # Prompt the user for child's name
         child_name = input("Enter the child's name: ")
-        birth_date = input("Enter the child's birth date (YYYY-MM-DD): ")
-        child_profile = ChildProfile(child_name, birth_date)
+        # New ChildProfile object with the child's name
+        child_profile = ChildProfile(child_name)
         print(f"Profile for {child_name} has been created.\n")
         return child_profile
     
+    # Static method to remove existing profile
     @staticmethod
     def remove_profile():
         child_name = input("Enter the child's name for the profile to remove: ").lower()
+        # Check if the profile directory exists
         if os.path.exists(child_name):
+            # Give user a warning that the profile will be deleted
             confirm = input(f"Are you sure you want to delete the profile for {child_name}? (y/n): ").lower()
+            # Remove profile directory via shutil 
             if confirm == 'y':
                 shutil.rmtree(child_name)
                 print(f"Profile for {child_name} has been removed.\n")
@@ -33,11 +39,13 @@ class ChildProfile:
                 print("Profile removal canceled.\n")
         else:
             print("Profile not found. Please try again.\n")
-
+    
+    # Static method to log in to an existing profile
     @staticmethod
     def login():
         while True:
             child_name = input("Enter the child's name for the profile: ").lower()
+            # Verify that profile directory exists and return profile/directory name
             if os.path.exists(child_name):
                 print(f"Logged in as {child_name}.\n")
                 return ChildProfile(child_name, None)
